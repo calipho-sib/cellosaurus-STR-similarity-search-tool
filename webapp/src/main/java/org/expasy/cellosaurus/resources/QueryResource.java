54fc3262cf7c6cb75ba9cfdaeff7a048923c8464
Thibault Robin <vynskir@gmail.com>
2019-02-12 11:17:16 +0100

    Added exception handling and merge query and results methods

--- webapp/src/main/java/org/expasy/cellosaurus/resources/QueryResource.java
+++ webapp/src/main/java/org/expasy/cellosaurus/resources/QueryResource.java
@@ -1,37 +1,86 @@
 package org.expasy.cellosaurus.resources;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.expasy.cellosaurus.Manager;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.Map;
 
 @Path("/query")
 public class QueryResource {
 
     @GET
+    @Produces({"application/json", "text/csv"})
     public Response get(@Context UriInfo info) {
-        MultivaluedMap<String, String> map = info.getQueryParameters();
+        try {
+            MultivaluedMap<String, String> map = info.getQueryParameters();
 
-        String type = "application/json";
-        String content = "inline";
+            String type = "application/json";
+            String content = "inline";
 
-        if (map.containsKey("format")) {
-            if (map.getFirst("format").equalsIgnoreCase("csv")) {
-                type = "text/csv";
-                content = "attachment; filename=Cellosaurus_STR_Results.csv";
+            if (map.containsKey("format")) {
+                if (map.getFirst("format").equalsIgnoreCase("csv")) {
+                    type = "text/csv";
+                    content = "attachment; filename=Cellosaurus_STR_Results.csv";
+                }
             }
-        }
-        String answer = Manager.search(map, type);
+            String answer = Manager.search(map, type);
 
-        return Response
-                .status(200)
-                .entity(answer)
-                .type(type)
-                .header("Content-Disposition", content)
-                .build();
+            return Response
+                    .status(200)
+                    .entity(answer)
+                    .type(type)
+                    .header("Content-Disposition", content)
+                    .build();
+
+        } catch (IllegalArgumentException e) {
+            return Response
+                    .status(400)
+                    .entity(e.toString())
+                    .type("text/plain")
+                    .build();
+        }
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces({"application/json", "text/csv"})
+    public Response post(String input) {
+        try {
+            MultivaluedMap<String, String> map = new MultivaluedHashMap<>();
+
+            String type = "application/json";
+            String content = "inline";
+
+            JsonObject object = new JsonParser().parse(input).getAsJsonObject();
+            for (Map.Entry<String, JsonElement> elements : object.entrySet()) {
+                map.add(elements.getKey(), elements.getValue().getAsString());
+
+                if (elements.getKey().equalsIgnoreCase("format")) {
+                    if (elements.getValue().getAsString().equalsIgnoreCase("csv")) {
+                        type = "text/csv";
+                        content = "attachment; filename=Cellosaurus_STR_Results.csv";
+                    }
+                }
+            }
+            String answer = Manager.search(map, type);
+
+            return Response
+                    .status(200)
+                    .entity(answer)
+                    .type(type)
+                    .header("Content-Disposition", content)
+                    .build();
+
+        } catch (IllegalArgumentException e) {
+            return Response
+                    .status(400)
+                    .entity(e.toString())
+                    .type("text/plain")
+                    .build();
+        }
     }
 }

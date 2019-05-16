ccc27ed202a00342275f663dfb945fcffe95f6a5
Thibault Robin <vynskir@gmail.com>
2019-05-16 15:48:52 +0200

    Update Javadoc

--- webapp/src/main/java/org/expasy/cellosaurus/formats/Writer.java
+++ webapp/src/main/java/org/expasy/cellosaurus/formats/Writer.java
@@ -12,7 +12,7 @@ public interface Writer {
 
     /**
      * @param json the relevant search information as a JSON {@code String}
-     * @throws IOException if the file cannot be written
+     * @throws IOException if an I/O exception occurs
      */
     default void add(String json) throws IOException {
         add(new JsonFormatter().toSearch(json));
@@ -20,12 +20,12 @@ public interface Writer {
 
     /**
      * @param search the relevant search information as a {@code Search} object
-     * @throws IOException if the file cannot be written
+     * @throws IOException if an I/O exception occurs
      */
     void add(Search search) throws IOException;
 
     /**
-     * @throws IOException if the file cannot be written
+     * @throws IOException if an I/O exception occurs
      */
     void write() throws IOException;
 

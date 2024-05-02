52cc70abd59cb3dfeb43790b4f1236ca715791a4
pmichel <pamichel@infomaniak.ch>
2024-05-02 11:50:38 +0200

    removed wrong import

--- backend/src/main/java/org/expasy/cellosaurus/formats/xml/XmlParser.java
+++ backend/src/main/java/org/expasy/cellosaurus/formats/xml/XmlParser.java
@@ -11,8 +11,6 @@ import org.expasy.cellosaurus.genomics.str.utils.ConflictResolver;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
-import javax.lang.model.util.Elements.Origin;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
